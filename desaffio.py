e26751e18e3fd53c84082cd6cb71c65bba908082
karol lima <kalima1998eu@gmail.com>
2022-07-11 19:43:49 -0300

    apaguei o codigo e corrige

--- desaffio.py
+++ desaffio.py
@@ -1,5 +1,3 @@
+#novoteste
 salario_mensal = int(input('Qual seu salario mensal? '))
-horas_trabalhadas = int(input('Quantas horas trabalhadas? '))
-valor_hora = int(input(salario_mensal / horas_trabalhadas))
-print(valor_hora)
-
+dsknsdfjsdfknksdj
